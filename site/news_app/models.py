cd048d1c2fdbae20dfe18d9a97d9c1e8868b6a05
damirfr <120688537+damirfr@users.noreply.github.com>
2024-02-16 17:19:43 +0600

    Add files via upload

---
+++ site/news_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField('заголовок', max_length = 50)
+    content = models.TextField('текст')
+    created_at = models.DateTimeField('дата публикации')
+    image = models.CharField('изображение', max_length = 200)
+    price = models.IntegerField('цена')
+
